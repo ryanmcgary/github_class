467c298a1d15b6c719c9534e9abaeef7d4e08fc6
Ryan McGary <ryan.mcgary@gmail.com>
2014-07-24 15:34:05 -0500

    added extendable module

--- index.js
+++ index.js
@@ -13,3 +13,31 @@ function anFunctionThatWillConflict (arg) {
 
 
 // BLAH BLAH BLAH
+
+var module = (function (window){
+  var localVariables, eAD, student;
+  self = this;
+
+  this.add = function (num) {
+    return num + num
+    // console.log("im called", num + num)
+  }
+  this.subtract = function (num) {
+    return num - num
+  }
+  this.multiply = function (num) {
+   return num * num
+  }
+  this.divide = function (num) {
+    return num / num
+  }
+  
+  return {
+    public: function (arr) {
+      return arr[0](arr[1])
+    },
+    extend: function (obj, num) {
+      return obj(num)
+    }
+  }
+})(window);
