e658f1222d06de289a3367d76d53f0cf5650b2d7
Ryan McGary <ryan.mcgary@gmail.com>
2014-07-24 15:33:23 -0500

    added a single function

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 // lets keep our stuff here
 
-
-
-
-
-
-
-
-
-
-
+function anFunctionThatWillConflict (arg) {
+  // This will be correct in master but a branch will be incorrect
+  // After branching this function, we will incorrectly update it in the branch
+  // Then we will fix the master!
+  // Then we will show both rebase and merge methods for resolving
+  Things = ["blah","blah","blah"]
+  for (var i = Things.length - 1; i >= 0; i--) {
+    Things[i]
+  };
+}
 
 
 // BLAH BLAH BLAH
-
